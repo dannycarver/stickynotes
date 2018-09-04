fb6fca559cd0cb149d0e7c5ea8ef5628606b283c
Danny Carver <sdannycarver@icloud.org>
2018-09-03 22:13:25 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'notes#index'
+
   get 'notes/index'
   get 'notes/show'
   get 'notes/new'
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   get 'notes/update'
   get 'notes/destroy'
   get 'notes/edit'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
